372bf2834d1eb201ef3ba61aa67e228d8c737bf2
cepherian <dpraz@hotmail.com>
2017-01-05 18:08:51 +1100

    weapon framework
    possible weapon framework

---
+++ Assets/abstractWeapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class abstractWeapon : MonoBehaviour {
+
+	public int ammo;
+
+	public abstract int ammoCapacity { get; }
+	public abstract float fireRate { get; }
+	public abstract float reloadDelay { get; }
+	public abstract float damage { get; }
+	/*
+		derived:
+		public override string MyConst {
+    		get { return "constant"; }
+		}
+	*/
+
+	public void fire(){
+		
+	}
+
+}
