bd00c94b0e07e4e718508c138a057a95f87abb9f
CloneWorks <cloneworks24@gmail.com>
2017-01-09 14:46:45 +1100

    Lobby
    Added a lobby thing.

--- Assets/scripts/localPlayer.cs
+++ Assets/scripts/localPlayer.cs
@@ -7,6 +7,9 @@ public class localPlayer : NetworkBehaviour {
     [SyncVar] //pushes this out to all clients from the server (server to client)
     public string playerName = "Player";
 
+    [SyncVar]
+    public Color playerColour = Color.white;
+
     //[SyncVar]
     //public Animator anim;
 
@@ -17,11 +20,11 @@ public class localPlayer : NetworkBehaviour {
     void OnGUI()
     {
         if(isLocalPlayer){
-            playerName = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), playerName);
-            if (GUI.Button(new Rect(130, Screen.height - 40, 80, 30), "change"))
-            {
-                CmdChangeName(playerName);
-            }
+            //playerName = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), playerName);
+            //if (GUI.Button(new Rect(130, Screen.height - 40, 80, 30), "change"))
+            //{
+            //    CmdChangeName(playerName);
+            //}
         }
     }
 
@@ -41,9 +44,14 @@ public class localPlayer : NetworkBehaviour {
             //GetComponent<playerCamera>().enabled = true;
 
             GetComponent<IK_WallClimb>().enabled = true;
+ 
         }
 
+        Renderer rend = GetComponentInChildren<Renderer>();
 
+        rend.material.SetColor("_OutlineColor", playerColour);
+
+        this.transform.position = new Vector3(Random.Range(-20,20), 0  , Random.Range(-20,20));
 	}
 	
 	// Update is called once per frame
---
+++ Assets/scripts/networkLobbyHook.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Prototype.NetworkLobby;
+
+public class networkLobbyHook : LobbyHook {
+
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+        localPlayer localPlayer = gamePlayer.GetComponent<localPlayer>();
+
+        localPlayer.playerName = lobby.playerName;
+        localPlayer.playerColour = lobby.playerColor;
+    }
+}
